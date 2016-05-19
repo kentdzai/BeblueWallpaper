17fd325671fefb279fc7c8de0b34fe9e97155092
kentdzai <kentdzai@gmail.com>
2016-05-19 11:16:48 +0700

    update detailimage

--- app/src/main/java/com/soft/kent/bebluewallpaper/DetailImageActivity2.java
+++ app/src/main/java/com/soft/kent/bebluewallpaper/DetailImageActivity2.java
@@ -1,10 +1,13 @@
 package com.soft.kent.bebluewallpaper;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -41,7 +44,8 @@ public class DetailImageActivity2 extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            );
         }
     };
     private View mControlsView;
@@ -72,12 +76,19 @@ public class DetailImageActivity2 extends AppCompatActivity {
             return false;
         }
     };
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_detail_image2);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationIcon(R.mipmap.ic_backarrow);
+        toolbar.setTitle("");
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
@@ -118,9 +129,9 @@ public class DetailImageActivity2 extends AppCompatActivity {
 
     private void hide() {
         // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+//        ActionBar actionBar = getSupportActionBar();
+        if (toolbar != null) {
+            toolbar.hideOverflowMenu();
         }
         mControlsView.setVisibility(View.GONE);
         mVisible = false;
@@ -149,5 +160,16 @@ public class DetailImageActivity2 extends AppCompatActivity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(DetailImageActivity2.this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
